dd5754d2d96151388c1e5b44a84fe02bd0b170fa
Halanna Lima <104534961+lanninha@users.noreply.github.com>
2024-07-12 17:09:37 -0300

    Olho completo

--- app/src/main/java/com/example/cookies/MainActivity2.kt
+++ app/src/main/java/com/example/cookies/MainActivity2.kt
@@ -6,13 +6,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var textoSenha: EditText
+    private lateinit var imagemMostrarSenha: ImageView
+
+    private var senhaVisivel:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        
+
+        textoSenha = findViewById(R.id.editTextTextPassword)
+        imagemMostrarSenha = findViewById(R.id.imagemMostrarSenha)
+
+        imagemMostrarSenha.setOnClickListener {
+            senhaVisivel = !senhaVisivel
+            alternarVisibilidadeSenha()
+        }
+
         supportActionBar?.hide()
         window.statusBarColor = Color.parseColor("#EFD6C1")
+    }
+
+    private fun alternarVisibilidadeSenha() {
+        if (senhaVisivel) {
+            // Mostrar senha
+            textoSenha.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            imagemMostrarSenha.setImageResource(R.drawable.eye)
+        } else {
+            // Esconder senha
+            textoSenha.transformationMethod = PasswordTransformationMethod.getInstance()
+            imagemMostrarSenha.setImageResource(R.drawable.invisibleeye)
+        }
+
+        // Move o cursor para o final do texto
+        textoSenha.setSelection(textoSenha.text.length)
     }
 }
--- app/src/main/java/com/example/cookies/MainActivity3.kt
+++ app/src/main/java/com/example/cookies/MainActivity3.kt
@@ -6,13 +6,53 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var textoSenha1: EditText
+    private lateinit var textoSenha2: EditText
+    private lateinit var imagemMostrarSenha1: ImageView
+    private lateinit var    imagemMostarSenha2 : ImageView
+
+    private var senhaVisivel1:Boolean = false
+    private var senhaVisivel2:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
+        textoSenha1 = findViewById(R.id.editTextTextPassword1)
+        imagemMostrarSenha1 = findViewById(R.id.imagemMostrarSenha1)
+
+        textoSenha2 = findViewById(R.id.editTextTextPassword2)
+        imagemMostarSenha2 = findViewById(R.id.imagemMostrarSenha2)
+
+        imagemMostrarSenha1.setOnClickListener {
+            senhaVisivel1 = !senhaVisivel1
+            alternarVisibilidadeSenha(textoSenha1,imagemMostrarSenha1,senhaVisivel1)
+        }
+
+        imagemMostarSenha2.setOnClickListener {
+            senhaVisivel2 = !senhaVisivel2
+            alternarVisibilidadeSenha(textoSenha2 , imagemMostarSenha2,senhaVisivel2)
+        }
         supportActionBar?.hide()
         window.statusBarColor = Color.parseColor("#EFD6C1")
+    }
+    private fun alternarVisibilidadeSenha(textoSenha :EditText , imagemMostrarSenha: ImageView, senhaVisivel:Boolean) {
+        if (senhaVisivel) {
+            // Mostrar senha
+            textoSenha.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            imagemMostrarSenha.setImageResource(R.drawable.eye)
+        } else {
+            // Esconder senha
+            textoSenha.transformationMethod = PasswordTransformationMethod.getInstance()
+            imagemMostrarSenha.setImageResource(R.drawable.invisibleeye)
+        }
+
+        // Move o cursor para o final do texto
+        textoSenha.setSelection(textoSenha.text.length)
     }
 }
